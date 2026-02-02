e0330f4314fcc68f5bb3b4ae4e33eecbc7a036a1
SnehaAdki <adkisneha1201@gmail.com>
2026-02-02 22:00:00 +0530

    Add files via upload

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import models
 from database import engine
-from routers import todos,auth
+from routers import todos,auth,admin,user
 
 
 app = FastAPI()
@@ -10,3 +10,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(todos.router)
 app.include_router(auth.router)
+
+app.include_router(admin.router)
+
+app.include_router(user.router)
