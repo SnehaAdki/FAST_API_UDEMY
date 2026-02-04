d9f8e2f8bcea82ccbb5963e83d04c4b20aa6ed54
SnehaAdki <adkisneha1201@gmail.com>
2026-02-04 22:52:07 +0530

    Add files via upload

--- ToDoApp/routers/admin.py
+++ ToDoApp/routers/admin.py
@@ -2,10 +2,15 @@ from email.policy import default
 
 from fastapi import Depends, APIRouter, HTTPException, Path
 from models import Todos
-from database import sessionLocal
+from database import sessionLocal,engine
 from typing import Annotated
 from sqlalchemy.orm import Session
 from starlette import status
+from sqlalchemy.sql import text
+from typing import Union
+from sqlalchemy.sql import Selectable
+from sqlalchemy.engine import Connection, Engine
+import pandas as pd
 from pydantic import BaseModel ,Field
 
 
@@ -19,6 +24,19 @@ router = APIRouter(
     tags=["admin"],
 )
 
+def get_engine_connection():
+    db_connection = None
+    try:
+        db = engine
+        db_connection = db.connect()
+        yield db_connection
+    # except Exception as ex:
+    #     raise HTTPException(status_code=status.WS_1011_INTERNAL_ERROR , detail='Connection Error')
+    finally:
+        # print("Closing the connection")
+        db_connection.close()
+
+
 def get_db():
     db = sessionLocal()
     try:
@@ -31,9 +49,23 @@ def get_db():
 
 db_dependency = Annotated[Session , Depends(get_db)]
 user_dependency = Annotated[Session , Depends(get_current_user)]
+engine_connection = Annotated[Session , Depends(get_engine_connection)]
+
+def get_detials(selectable: Union[Selectable, str], connection: Union[Connection, Engine]) -> pd.DataFrame:
+    return pd.read_sql(selectable,connection)
+
+# via connection_engine dependency
+@router.get("/all_todos_all_users",status_code=status.HTTP_200_OK)
+async def read_all_todos_all_users(user:user_dependency, db_engine: engine_connection ):
+    if user is None or user.get('role') != 'admin':
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED , detail="Authentication Failed")
+    query = text("""select * from todos""")
+    val =  get_detials(selectable=query, connection=db_engine)
+    return  val.to_dict(orient='records')
 
 
-@router.get("/abc",status_code=status.HTTP_200_OK)
+# via db sessionmaker dependency
+@router.get("/all_todos",status_code=status.HTTP_200_OK)
 async def read_all(user:user_dependency, db: db_dependency ):
     if user is None or user.get('role') != 'admin':
         raise HTTPException(status.HTTP_401_UNAUTHORIZED , detail="Authentication Failed")
