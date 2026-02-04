a4440e1a155ffdf8a6b7a24752c4d8d4998fd52f
SnehaAdki <adkisneha1201@gmail.com>
2026-02-04 22:51:42 +0530

    Add files via upload

--- ToDoApp/database.py
+++ ToDoApp/database.py
@@ -2,9 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+## sqlite connection
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread' : False})
+#postgress
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost/TodoApplicationDatabase'
+
+# mysql connection
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:test1234@localhost:3306/TodoApplicationDatabase'
+
+## engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread' : False}) # used for the sqlite
+# engine = create_engine(SQLALCHEMY_DATABASE_URL) used for the postgress
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 sessionLocal = sessionmaker(autoflush=False,autocommit = False, bind= engine)
 
