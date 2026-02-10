ec66d13aea9c279d79f686b84eeef36fd20f777d
SnehaAdki <adkisneha1201@gmail.com>
2026-02-10 22:14:53 +0530

    Add files via upload

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -8,9 +8,12 @@ app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+@app.get("/healthy")
+def heal_check():
+    return {'status': 'Healthy'}
+
+
 app.include_router(todos.router)
 app.include_router(auth.router)
-
 app.include_router(admin.router)
-
 app.include_router(user.router)
